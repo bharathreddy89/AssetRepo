467e096fbde1752c481193d15992dd3229666cd5
bharathreddy89 <m.bharath49@gmai.com>
2020-04-17 15:35:32 +0530

    Update DeployCustomerImageToK8.sh

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,5 +2,5 @@
 #
 
 cd /opt/rancher
-./rancher login https://k8s.eur.ad.sag/v3 --token token-2glqs:mvvb6ck8sw7kk8klx288prpprqdhgvv9lpp7btvvmxfvxgl9stwgd6
+./rancher login https://k8s.eur.ad.sag/v3 --token token-cczbt:9shcpr4l68vxz5hfhn9n7wlbgv6p7nxld7lh78kz5z2ptg8q9w67vr
 ./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml
